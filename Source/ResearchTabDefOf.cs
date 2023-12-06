dfc5845b51b4979a1426de9941d6a859be2d00a8
nils <nils130902@gmail.com>
2023-12-06 07:03:33 +0100

    Add project files.

---
+++ Source/ResearchTabDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+
+namespace TechTree {
+    [DefOf]
+    public static class ResearchTabDefOf {
+        public static ResearchTabDef Undefined;
+        public static ResearchTabDef Animal;
+        public static ResearchTabDef Neolithic;
+        public static ResearchTabDef Medieval;
+        public static ResearchTabDef Industrial;
+        public static ResearchTabDef Spacer;
+        public static ResearchTabDef Ultra;
+        public static ResearchTabDef Archotech;
+    }
+}
---
+++ Source/TechTree.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace TechTree {
+    [StaticConstructorOnStartup]
+    public static class TechTree {
+        static TechTree() {
+            List<ResearchProjectDef> researchProjects = DefDatabase<ResearchProjectDef>.AllDefsListForReading;
+            researchProjects.ForEach(researchProjectDef => {
+                TechLevel techLevel = researchProjectDef.techLevel;
+                ResearchTabDef newTab = techLevel switch {
+                    TechLevel.Animal => ResearchTabDefOf.Animal,
+                    TechLevel.Neolithic => ResearchTabDefOf.Neolithic,
+                    TechLevel.Medieval => ResearchTabDefOf.Medieval,
+                    TechLevel.Industrial => ResearchTabDefOf.Industrial,
+                    TechLevel.Spacer => ResearchTabDefOf.Spacer,
+                    TechLevel.Ultra => ResearchTabDefOf.Ultra,
+                    TechLevel.Archotech => ResearchTabDefOf.Archotech,
+                    _ => ResearchTabDefOf.Undefined
+                };
+                researchProjectDef.tab = newTab;
+            });
+
+            //List<ResearchTabDef> populatedTabs = [];
+            //DefDatabase<ResearchProjectDef>.AllDefsListForReading.ForEach(researchProjectDef => {
+            //    ResearchTabDef tab = researchProjectDef.tab;
+            //    if (!populatedTabs.Contains(tab)) {
+            //        populatedTabs.Add(tab);
+            //    }
+            //});
+        }
+    }
+}
